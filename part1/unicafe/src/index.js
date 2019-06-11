b22b38c7259e87ba11a58b3d35ba15e174a01189
Alvar Salokangas <saloalv@live.com>
2019-06-11 22:10:32 +0300

    step6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,16 +7,16 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
-    const incGood = () => {setGood(good + 1)};
-    const incNeutral = () => {setNeutral(neutral + 1)};
-    const incBad = () => {setBad(bad + 1)};
+    const incGood = () => { setGood(good + 1) };
+    const incNeutral = () => { setNeutral(neutral + 1) };
+    const incBad = () => { setBad(bad + 1) };
 
     return (
         <div>
             <h1>give feedback</h1>
-            <Button text="good" onClick={incGood}/>
-            <Button text="neutral" onClick={incNeutral}/>
-            <Button text="bad" onClick={incBad}/>
+            <Button text="good" onClick={incGood} />
+            <Button text="neutral" onClick={incNeutral} />
+            <Button text="bad" onClick={incBad} />
             <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
@@ -40,21 +40,30 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
         <div>
             <h1>statistics</h1>
-            <Statistic text="good" value={good}/> <br />
-            <Statistic text="neutral" value={neutral}/> <br />
-            <Statistic text="bad" value={bad} /> <br />
-            <Statistic text="average" value={average} /> <br />
-            <Statistic text="positive" value={positivePercentage} /> <br />
+            <table>
+                <tbody>
+                    <Statistic text="good" value={good} />
+                    <Statistic text="neutral" value={neutral} />
+                    <Statistic text="bad" value={bad} />
+                    <Statistic text="average" value={average} />
+                    <Statistic text="positive" value={positivePercentage} />
+                </tbody>
+            </table>
         </div>
     );
 }
 
 const Button = ({ text, onClick }) => {
-    return <> <button onClick={onClick} >{text}</button></>
+    return <button onClick={onClick} >{text}</button>
 }
 
 const Statistic = ({ text, value }) => {
-    return <>{text} {value}</>
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
 }
 
 ReactDOM.render(<App />,
