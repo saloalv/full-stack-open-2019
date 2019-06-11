b2cf4c1037a3c335a43ea4ba261c1fd00c102511
Alvar Salokangas <saloalv@live.com>
2019-06-11 18:10:52 +0300

    more playing around

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-    <div>
-        <p>Hello World</p>
-    </div>
-)
+const App = () => {
+    const now = new Date();
+    console.log(now);
+    const a = 5;
+    const b = 37;
+    return (
+        <>
+            <Hello name="world"/>
+            <Hello name="test"/>
+            <p>The current date is {now.toString()}</p>
+            <p>{a} plus {b} is {a + b}</p>
+        </>
+    )
+}
+
+const Hello = (props) => {
+    return (
+        <>
+            <p>Hello {props.name}!</p>
+        </>
+    )
+}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
