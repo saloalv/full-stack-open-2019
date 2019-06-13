a75ca966c26c2b028960ceff839daf10dc4762dd
Alvar Salokangas <saloalv@live.com>
2019-06-13 15:50:35 +0300

    fetch data from json server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import FilteredPersonTable from './components/FilteredPersonTable';
 import AddPersonForm from './components/AddPersonForm';
 import FilterFragment from './components/FilterFragment';
+
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [currentFilter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => setPersons(response.data))
+  },[])
 
   const addName = (event) => {
     event.preventDefault()
